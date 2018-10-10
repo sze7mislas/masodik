26a71ec4a5ac54a964125381c50abb7b76acc9b3
Miskolczi László <miska0627@gmail.com>
2018-10-10 18:34:20 +0200

    fdsf

--- Oktober_10/src/Main.java
+++ Oktober_10/src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// első feladat elkészült
+		// első feladat elkészültdsf
 		for (int i = 0; i < args.length; i++) {
 			System.out.println(args[i]);
 		}
