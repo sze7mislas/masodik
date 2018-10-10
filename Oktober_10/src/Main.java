471d2e55a31f56b6f4efdf25a1bf546948ce1200
Miskolczi László <miska0627@gmail.com>
2018-10-10 18:21:54 +0200

    elso feladat kész

--- Oktober_10/src/Main.java
+++ Oktober_10/src/Main.java
@@ -2,12 +2,11 @@
 public class Main {
 
 	public static void main(String[] args) {
-		//első feladat
-		for (int i=0;i<args.length;i++)
-		{
+		// első feladat
+		for (int i = 0; i < args.length; i++) {
 			System.out.println(args[i]);
 		}
-		
+
 	}
 
 }
