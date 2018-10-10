f8c22cba01720382206bc50a809290785e8680fe
Miskolczi László <miska0627@gmail.com>
2018-10-10 18:32:39 +0200

    elsp

--- Oktober_10/src/Main.java
+++ Oktober_10/src/Main.java
@@ -2,10 +2,12 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// első feladat
+		// első feladat elkészült
 		for (int i = 0; i < args.length; i++) {
 			System.out.println(args[i]);
 		}
+		
+		
 
 	}
 
