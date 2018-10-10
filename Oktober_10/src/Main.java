ef3c6fcc018d31fa245dbed3cfbe9327c5113971
Miskolczi László <miska0627@gmail.com>
2018-10-10 18:54:56 +0200

    3 feladat kész

--- Oktober_10/src/Main.java
+++ Oktober_10/src/Main.java
@@ -6,8 +6,21 @@ public class Main {
 		for (int i = 0; i < args.length; i++) {
 			System.out.println(args[i]);
 		}
+		// 2.feladat
+		for (int i = 1; i < 20; i++)
+			if (i % 2 == 0)
+			{
+				System.out.println(i);
+			}
+		//3.feladat
+		int a=Integer.parseInt(args[0]);
+		int b=Integer.parseInt(args[1]);	
 		
-		
+		for (int i = a; i < b; i++)
+			if (i % 3 == 0)
+			{
+				System.out.println(i);
+			}
 
 	}
 
